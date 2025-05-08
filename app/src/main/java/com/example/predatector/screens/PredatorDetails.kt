3bd3ec29e2f6a615833c470477e553f811ca9f50
ChillyDealer <49028947+ChillyDealer@users.noreply.github.com>
2025-05-08 12:02:54 +0200

    details page

--- app/src/main/java/com/example/predatector/screens/PredatorDetails.kt
+++ app/src/main/java/com/example/predatector/screens/PredatorDetails.kt
@@ -1,11 +1,16 @@
 package com.example.predatector.screens
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,10 +21,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.ui.graphics.Color
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
@@ -59,10 +66,17 @@ fun Greeting(name: String, PredatorInfo: List<String>, imageId: Int, modifier: M
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
-            .padding(16.dp)
-    ){
+            .padding(start = 36.dp, 56.dp, 36.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text("Predator name:", fontSize = 24.sp)
-        Text(text = "$name", fontSize = 24.sp, fontWeight = FontWeight.Black, color = Color.Black, modifier = Modifier.padding(bottom = 20.dp))
+        Text(
+            text = "$name",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Black,
+            color = Color.Black,
+            modifier = Modifier.padding(bottom = 20.dp)
+        )
         imagePainter?.let {
             Image(
                 painter = it,
@@ -74,11 +88,37 @@ fun Greeting(name: String, PredatorInfo: List<String>, imageId: Int, modifier: M
             )
         }
 
-        Card (modifier = Modifier.fillMaxWidth(), shape = RoundedCornerShape(10.dp)) {
-            Column {
-                Text("Predator info:")
-                PredatorInfo.forEach { predatorsInfo ->
-                    Text("# $predatorsInfo")
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = Color.Transparent),
+            elevation = CardDefaults.cardElevation(0.dp)
+        ) {
+            Box(modifier = Modifier.padding(12.dp)) {
+                // two columns
+                val leftColumn = PredatorInfo.filterIndexed { index, _ -> index % 2 == 0 }
+                val rightColumn = PredatorInfo.filterIndexed { index, _ -> index % 2 == 1 }
+
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(top = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        leftColumn.forEach { item ->
+                            Text(text = "- $item", fontSize = 16.sp)
+                        }
+                    }
+
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        rightColumn.forEach { item ->
+                            Text(text = "- $item", fontSize = 16.sp)
+                        }
+                    }
                 }
             }
         }
